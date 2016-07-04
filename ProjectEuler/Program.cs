959aa19323fc28fb3452a7964c04b452d1206fa8
Huy Cuong <lazivietboi@gmail.com>
2016-07-05 05:56:57 +0700

    first commit

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem_1 problem_1 = new Problem_1();
+            problem_1.solveProblem_1();
+        }
+    }
+}
